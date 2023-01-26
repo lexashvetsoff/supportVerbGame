b31baa65f74e22dd05f18b335b7493174c763056
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-01-26 21:59:24 +0800

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -5,15 +5,42 @@ from dotenv import load_dotenv
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from google.cloud import dialogflow
+from google.oauth2 import service_account
+
 load_dotenv()
 VK_TOKEN = os.environ['VK_TOKEN']
+PROJECT_ID = os.environ['PROJECT_ID']
+GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+credentials = service_account.Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
+intents_client = dialogflow.IntentsClient(credentials=credentials)
 
 
-def echo(event, vk_api):
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={
+            'session': session,
+            'query_input': query_input
+        }
+    )
+
+    return response
+
+
+def answer(event, vk_api):
+    session_id = event.user_id
+    answer = detect_intent_texts(PROJECT_ID, session_id, event.text, 'ru')
     vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        user_id = session_id,
+        message = answer.query_result.fulfillment_text,
+        random_id = random.randint(1,1000)
     )
 
 
@@ -24,7 +51,7 @@ def main():
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            answer(event, vk_api)
 
 
 if __name__  == '__main__':
