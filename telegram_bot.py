cb9919af36b5413235b2248759e6089e32e331d4
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-01-31 10:41:40 +0800

    убраны лишние коментарии

--- telegram_bot.py
+++ telegram_bot.py
@@ -48,16 +48,11 @@ def main():
 
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
-
-    # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
 
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
